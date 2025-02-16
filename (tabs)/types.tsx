ff824400927844e0d98cd7caae0c09a74255602a
katyM9 <114771093+katyM9@users.noreply.github.com>
2025-02-15 15:52:31 -1200

    Delete (tabs) directory

--- (tabs)/types.tsx
+++
@@ -1,9 +0,0 @@
-// types.ts
-export type RootStackParamList = {
-    Alergias: undefined;  // La pantalla de Alergias no recibe parámetros
-    AgregarAlergias: { alergia?: any };  // La pantalla de AgregarAlergias puede recibir un objeto "alergia"
-
-    ChequeoDiario: undefined;
-    AgregarChequeoDiario: { chequeo?: any };  // La pantalla de AgregarChequeoDiario puede recibir un chequeo
-  };
-  
